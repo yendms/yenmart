653fce9e0048463c27d6b82dd12880bcb1aeb891
Yen Adams Sokama-Neuyam <yen@Scholarsup.com>
2014-08-19 00:28:25 +0200

    Devise customized

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Etsydemo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #remember to change domain name to actual domain 
+  config.action_mailer.default_url_options = {:host =>'localhost:3000'}
 end
